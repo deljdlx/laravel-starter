3b614f6932cf9fde5e730551b8f6e02b852425e2
julien delsescaux <jdelsescaux@stimdata.com>
2025-11-13 20:03:44 +0100

    test livewire

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,6 +1,6 @@
 <div style="text-align: center">
     <x-card title="Livewire Counter Example">
         <button wire:click="increment" style="font-size:2rem; padding:1rem 2rem;">+1</button>
-        <h2 style="margin-top:1rem;">Valeur : {{ $count }}</h2>
+    <h2 style="margin-top:1rem; color: {{ $count % 2 === 0 ? 'blue' : 'red' }};">Valeur : {{ $count }}</h2>
     </x-card>
 </div>
