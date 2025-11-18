7e2f9b4140959fedc036f55e76939c5bdfc01ea8
julien delsescaux <jdelsescaux@stimdata.com>
2025-11-18 19:40:57 +0100

    sync

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -4,11 +4,31 @@ namespace App\Models;
 use ApiPlatform\Metadata\ApiResource;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Spatie\ModelStatus\HasStatuses;
 
+// properties list
+/**
+ * 
+ * @property ?int $id
+ * @property string $title
+ * @property ?string $description
+ * @property int $user_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ */
 #[ApiResource]
 class Task extends Model
 {
     /** @use HasFactory<\Database\Factories\TaskFactory> */
     use HasFactory, HasStatuses;
+
+
+    /**
+     * @return BelongsTo<User>
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
